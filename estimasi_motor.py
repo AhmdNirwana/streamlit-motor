8b369826e8815771b21dba8a84b6bc9e4d0fab05
Ahmad Nirwana <121556179+AhmdNirwana@users.noreply.github.com>
2023-04-12 16:24:01 +0700

    Add files via upload

---
+++ estimasi_motor.py
@@ -0,0 +1,17 @@
+import pickle
+import streamlit as st
+
+lrmodel = pickle.load(open('estimasi_motor.sav', 'rb'))
+
+st.title('Estimasi Harga Motor Bekas')
+
+selling_price = st.number_input('Input Harga Jual')
+year = st.number_input('Input Tahun Motor')
+
+predict = ''
+
+if st.button('Estimasi Harga') :
+    predict = lrmodel.predict(
+        [[selling_price,year]]
+    )
+    st.write ('Estimasi Harga Motor Bekas Dalam Pounds : ', predict)
